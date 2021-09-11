4dfadcf738753a9ad8608e24d9a20ffd1e44ae93
ManuelHamana <mhamana@strappinc.net>
2021-09-11 12:20:43 -0400

    avance

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.css'
+
+function NavBar() {
+    return (
+        <header>
+            <h1>Logo</h1>
+            <nav>
+                <a href="#">Enlace1</a>
+                <a href="#">Enlace2</a>
+                <a href="#">Enlace3</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
---
+++ src/components/Saludo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Saludo({name}){
+    console.log("Coders");
+    return (
+        <>
+            <h1>Hola  {name}!</h1>
+            
+        </>
+    )
+}
+
+export default Saludo;
